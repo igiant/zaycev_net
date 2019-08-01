28a84219301fffd0397607346b5e80556d3ee46e
Sergey Grishayev <sergey.grishayev@gmail.com>
2019-08-01 11:38:28 +0400

    добавление тестов на createAddr

--- main_test.go
+++ main_test.go
@@ -23,12 +23,28 @@ func TestGetRange(t *testing.T) {
 	for _, test := range arrTests {
 		resMin, resMax = getRange(test.value)
 		if resMin != test.min || resMax != test.max {
-			t.Errorf("Ожидалось (%d, %d), пришло (%d, %d) = getRange(%s)",
+			t.Errorf("Ожидалось (%d, %d), получили (%d, %d) = getRange(%s)",
 				test.min, test.max, resMin, resMax, test.value)
 		}
 	}
 }
 
-func TestParser(t *testing.T) {
+func TestCreateAddr(t *testing.T) {
+	result := createAddr("http", "zaycev.net", "search.html", "one two three", -1)
+	if result != "http://zaycev.net/search.html?query_search=one+two+three" {
+		t.Errorf("Ожидалось %s, получили %s", result, "http://zaycev.net/search.html?query_search=one+two+three")
+	}
+	result = createAddr("http", "zaycev.net", "search.html", "один+два+три", 2)
+	if result != "http://zaycev.net/search.html?page=2&query_search=%D0%BE%D0%B4%D0%B8%D0%BD%2B%D0%B4%D0%B2%D0%B0%2B%D1%82%D1%80%D0%B8" {
+		t.Errorf("Ожидалось %s, получили %s", result,
+			"http://zaycev.net/search.html?page=2&query_search=%D0%BE%D0%B4%D0%B8%D0%BD%2B%D0%B4%D0%B2%D0%B0%2B%D1%82%D1%80%D0%B8")
+	}
+	result = createAddr("https", "zaycev.net", "search.html", "123", 1)
+	if result != "https://zaycev.net/search.html?query_search=123" {
+		t.Errorf("Ожидалось %s, получили %s", result, "https://zaycev.net/search.html?query_search=123")
+	}
+}
+
+func TestParse(t *testing.T) {
 	//
 }
